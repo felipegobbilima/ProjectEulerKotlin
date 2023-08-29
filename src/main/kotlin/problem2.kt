d0b73c9dca73e35e173b5f2714ff891ff30cd2fa
Felipe Gobbi <fgobbi29@gmail.com>
2023-08-29 10:50:45 -0300

    third commit

---
+++ src/main/kotlin/problem2.kt
@@ -0,0 +1,22 @@
+
+
+fun main() {
+
+    fun fib(n: Int): Int {
+        if (n == 1) return 1
+        if (n == 2) return 2
+
+        return fib(n - 1) + fib(n - 2)
+
+    }
+
+    var soma = 0
+
+    for (n in 1 until 4000000) {
+        val fib = fib(n)
+        if (fib >= 4000000) break
+        if (fib % 2 == 0) soma += fib
+
+    }
+    println("total:${soma}")
+}
