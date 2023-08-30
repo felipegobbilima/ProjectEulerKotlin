800b15877f91b24227c9132887d9ccfa13c185df
Felipe Gobbi <fgobbi29@gmail.com>
2023-08-30 10:37:56 -0300

    thirdy commit

---
+++ src/main/kotlin/problem4.kt
@@ -0,0 +1,25 @@
+fun main(){
+    var palindrome = f (100, 999)
+    println("$palindrome")
+}
+
+fun f(comeco: Int, fim: Int): Int{
+
+    var palindrome = 0
+    for(i in comeco .. fim){
+        for (j in i .. fim){
+            val product = i * j
+            if (n(product) && product > palindrome) {
+                palindrome = product
+            }
+        }
+    }
+    return palindrome
+
+}
+fun n(number: Int): Boolean {
+    val soma = number.toString()
+    return soma == soma.reversed()
+
+}
+
